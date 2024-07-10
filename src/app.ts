4f2ee7729a6834515218f16bf5c25681c6d682a8
clickbait-4587 <clickbait4587@gmail.com>
2024-07-10 10:13:17 +0200

    BOUT TO DEMO-TEST UMA#2

--- src/app.ts
+++ src/app.ts
@@ -1,76 +1,75 @@
-import createError from 'http-errors';
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
-import authRouter from './routes/auth';
-import botsRouter from './routes/bots';
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
+import authRouter from "./routes/auth";
+import botsRouter from "./routes/bots";
 
 const app = express();
-import { default as mongoose } from 'mongoose';
-import cors from 'cors';
-import { DEV, jobs, setJobs } from './utils/constants';
-import dotenv from "dotenv"
-import { strategies } from './strategies';
-import { Bot } from './models';
-import { addBotJob } from './utils/orders/funcs';
-import { MainOKX } from './classes/okx';
+import { default as mongoose } from "mongoose";
+import cors from "cors";
+import { DEV, setJobs } from "./utils/constants";
+import dotenv from "dotenv";
+import { Bot } from "./models";
+import { addBotJob } from "./utils/orders/funcs";
 
-
-dotenv.config()
+dotenv.config();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug'); 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(cors()) 
- app.use(cors({ 
-  origin: '*'
-}))
+app.use(cors());
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 /*------------------ mongodb ----------------------- */
-async function connectMongo(){
-    let mongoURL = (DEV ? process.env.MONGO_URL_LOCAL : process.env.MONGO_URL )!
+async function connectMongo() {
+    let mongoURL = (DEV ? process.env.MONGO_URL_LOCAL : process.env.MONGO_URL)!;
     try {
-        console.log(mongoURL); 
-      await mongoose.connect(mongoURL, {dbName: "tb"});
-      console.log('Connection established'); 
+        console.log(mongoURL);
+        await mongoose.connect(mongoURL, { dbName: "tb" });
+        console.log("Connection established");
+    } catch (e) {
+        console.log("Could not establish connection");
+        console.log(e);
     }
-    catch(e) {
-      console.log('Could not establish connection')
-     console.log(e); 
-    }
-  }
-  (async function(){await connectMongo()})()
-  /*------------------ End mongodb ----------------------- */
-app.use(logger('dev'));
+}
+(async function () {
+    await connectMongo();
+})();
+/*------------------ End mongodb ----------------------- */
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/auth', authRouter);
-app.use('/bots', botsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/auth", authRouter);
+app.use("/bots", botsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
-
 
 /* import schedule, {Job} from "node-schedule"
 
@@ -93,14 +92,14 @@ const job = schedule.scheduleJob("* * * * * *", function(){
 
 jobs.push({job, id: "1"}) */
 
-const main = async ()=>{
-    const activeBots = await Bot.find({active: true}).exec()
-    setJobs([])
-    for (let bot of activeBots){
-        await addBotJob(bot)
+const main = async () => {
+    const activeBots = await Bot.find({ active: true }).exec();
+    setJobs([]);
+    for (let bot of activeBots) {
+        await addBotJob(bot);
     }
     //new MainOKX()
-}
+};
 
-main()
+main();
 export default app;
