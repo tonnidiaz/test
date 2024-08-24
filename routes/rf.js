a414be0932db0928acdcbc1a910eae21c6ff289f
clickbait-4587 <clickbait4587@gmail.com>
2024-08-24 20:54:48 +0200

    bytick

--- routes/rf.js
+++ routes/rf.js
@@ -13,10 +13,10 @@ router.get('/kline', async function(req, res, next) {
     demo = demo == 'true'
     const API_KEY = demo ? BYBIT_API_KEY_DEV : BYBIT_API_KEY
     const API_SECRET = demo ? BYBIT_API_SECRET_DEV : BYBIT_API_SECRET
-
+    const baseURL = demo ? "https://api-testnet.bybit.com" : "https://api.bytick.com"
     try{
         console.log({demo})
-        const client = new RestClientV5({key: API_KEY, secret: API_SECRET, demoTrading: demo, testnet: demo})
+        const client = new RestClientV5({key: API_KEY, secret: API_SECRET, demoTrading: demo, testnet: demo}, {baseURL})
 
         const symbol = "SOLUSDT", interval= 5, category = 'spot';
 
