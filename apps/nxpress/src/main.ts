fad5660ab0af555591be9d8848d99e5e3fd691d8
tonnidiaz <therealhackingknight@gmail.com>
2024-09-28 13:48:33 +0200

    init nx commit

--- apps/nxpress/src/main.ts
+++ apps/nxpress/src/main.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-
+import {sayHello} from '@_-nx-wp/shared-lib'
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
-
+sayHello("Johnathan")
 app.get('/', (req, res) => {
+    
   res.send({ message: 'Hello API' });
 });
 
---
+++ shared-lib/src/lib/shared-lib.ts
@@ -0,0 +1,5 @@
+export function sharedLib(): string {
+  return 'shared-lib';
+}
+
+export function sayHello(name: string){console.log(`Hello Mr/Mrs ${name}`)}
