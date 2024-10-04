be9c0de852b1b4e85a6eef7b44048a7a1c4be356
tonnidiaz <therealhackingknight@gmail.com>
2024-10-04 19:23:56 +0200

    eish

--- packages/tulib/src/lib/utils/funcs4.ts
+++ packages/tulib/src/lib/utils/funcs4.ts
@@ -3,17 +3,17 @@ import mongoose from "mongoose";
 
 config()
 
-export async function connectMongo(DEV = true) {
+export async function connectMongo(DEV = true, dbName = "tb") {
     console.log({ DEV });
     let mongoURL = (DEV ? process.env.MONGO_URL_LOCAL : process.env.MONGO_URL)!;
     try {
         console.log(mongoURL);
-        const mn = await mongoose.connect(mongoURL, { dbName: "tb" });
+        const mn = await mongoose.connect(mongoURL, { dbName, family: 4 });
         console.log("\nConnection established\n");
         return mn
     } catch (e) {
         console.log("Could not establish connection");
-        console.log(e);
+        return console.log(e);
     }
 }
 
