30e2fe1fb5e45d78ee046fcdcbcabc18e632ce68
tonnidiaz <therealhackingknight@gmail.com>
2024-11-14 19:48:40 +0200

    test-bitget

--- packages/common/src/utils/consts.ts
+++ packages/common/src/utils/consts.ts
@@ -1,5 +1,5 @@
 import { TestBinance } from "@cmn/classes/test-binance";
-// import { TestBitget } from "@cmn/classes/test-bitget";
+import { TestBitget } from "@cmn/classes/test-bitget";
 // import { TestKucoin } from "@cmn/classes/test-kucoin";
 // import { TestMexc } from "@cmn/classes/test-mexc";
 import { TestPlatform, TestBybit, TestOKX } from "@cmn/classes/test-platforms";
@@ -11,7 +11,7 @@ export const test_platforms : {[key: string] : typeof TestPlatform} = {
     // bybit: TestBybit ,
     // okx: TestOKX ,
     // kucoin: TestKucoin ,
-    // bitget: TestBitget,
+    bitget: TestBitget,
     // mexc: TestMexc,
 };
 
