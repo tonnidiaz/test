8da16fc3bd4287bd9dda543e0a266c476593f2d1
tonnidiaz <therealhackingknight@gmail.com>
2024-11-14 17:03:36 +0200

    rmvd some comps

--- apps/sv-app/src/components/TuField.svelte
+++
@@ -1,4 +0,0 @@
-<input class="input input-bordered" bind:value={value} {...props}>
-<script lang="ts">
-    let {value = $bindable(), ...props } = $props()
-</script>
--- apps/sv-app/src/routes/api/hello/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from "@sveltejs/kit"
-import type { RequestHandler } from "./$types"
-
-export const GET: RequestHandler = ({url})=>{
-    const name = url.searchParams.get("name") || "Anonymous"
-    const r = {hello: name}
-    return json(r)
-}
