d6c3fd440c667e9c5fe7a5fa33f6c6134ca728b5
tonnidiaz <therealhackingknight@gmail.com>
2025-01-05 03:04:06 +0200

    # mexc-apis and bitget apis

--- packages/tsmodule/src/utils/bend/conts.ts
+++ packages/tsmodule/src/utils/bend/conts.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import bignumber_js from "bignumber.js";
 // import binance from "binance";
 // import binance_api_node from "binance-api-node";
-import bitget_api from "bitget-api";
+// import bitget_api from "bitget-api";
 import bybit_api from "bybit-api";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -10,9 +10,9 @@ import gate_api from "gate-api";
 import indicatorts from "indicatorts";
 import jsonwebtoken from "jsonwebtoken";
 import * as kucoin_api from "kucoin-api";
-import mexc_api_sdk from "mexc-api-sdk";
+// import mexc_api_sdk from "mexc-api-sdk";
 import mongoose from "mongoose";
-import node_mexc_apis from "node-mexc-apis";
+// import node_mexc_apis from "node-mexc-apis";
 import node_schedule from "node-schedule";
 import nodemailer from "nodemailer";
 import okx_api from "okx-api";
@@ -24,17 +24,26 @@ const _data = {
     bignumber_js,
     // binance,
     // binance_api_node,
-    bitget_api,
+    // bitget_api,
     bybit_api,
     cors,
     dotenv,
     gate_api,
     indicatorts,
-    jsonwebtoken, kucoin_api, mexc_api_sdk, mongoose, node_mexc_apis, node_schedule, nodemailer, okx_api, socket_io
-}
+    jsonwebtoken,
+    kucoin_api,
+    // mexc_api_sdk,
+    mongoose,
+    // node_mexc_apis,
+    node_schedule,
+    nodemailer,
+    okx_api,
+    socket_io,
+};
 
 console.log({ meta });
-console.log({_data});
+console.log({ _data });
 export const getData = () => {
-    console.log({_data});
-    return Object.values(_data).map(el=> typeof el)}
+    console.log({ _data });
+    return Object.values(_data).map((el) => typeof el);
+};
